2ae8ab8ea5fd78907fb67262a1325895738060fd
acwink <acwink@163.com>
2022-08-17 22:41:17 +0800

    build(smarty-ui-vite/scripts): 实现组件分包，方便按需导入

--- packages/smarty-ui-vite/src/ACButton/index.ts
+++ packages/smarty-ui-vite/src/ACButton/index.ts
@@ -1,3 +1,13 @@
+import { App } from "vue";
 import ACButton from "./ACButton";
+import "uno.css";
 
-export default ACButton;
+// 导出组件ACButton
+export { ACButton };
+
+// 导出Vue插件
+export default {
+  install(app: App) {
+    app.component(ACButton.name, ACButton);
+  },
+};
